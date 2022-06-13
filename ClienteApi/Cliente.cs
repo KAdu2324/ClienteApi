2ffcd08dcdc6a1cb0f9f85a830bb1a35b483a5f0
AS <kadudu23@gmail.com>
2022-06-13 12:36:25 -0300

    Adicionar arquivos de projeto.

---
+++ ClienteApi/Cliente.cs
@@ -0,0 +1,16 @@
+﻿namespace ClienteApi
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = String.Empty;
+        public string CPF { get; set; } = String.Empty;
+
+        public string Email { get; set; } = String.Empty;
+
+        public string Endereço { get; set; } = String.Empty;
+
+        public string Contato { get; set; } = String.Empty;
+        public string Status { get; set; } = String.Empty;
+    }
+}
---
+++ ClienteApi/Controllers/ClienteController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClienteApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+      
+            private static List<Cliente> Cliente = new List<Cliente>
+            {
+                new Cliente {
+                    Id = 1,
+                    Nome = "Carlos",
+                    CPF= "12345678912",
+                    Contato= "1194856-5689",
+                    Email = "hgsdgs@gmail.com",
+                    Endereço = "rua junior",
+                    Status = "ativo",
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Cliente>>> Get()
+        { 
+
+            return Ok(Cliente);
+        }
+    }
+}
